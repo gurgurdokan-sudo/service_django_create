4d47a7c2c7f90834429ac11a0c7563aeeb9aaee1
yuya <gurgur.dokan@gmail.com>
2026-06-21 11:23:12 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        'ENGINE': '13.231.19.50:8001',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydb',
         'USER': 'myuser',
         'PASSWORD': 'mypassword',
