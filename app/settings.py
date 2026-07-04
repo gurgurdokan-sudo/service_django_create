cb9a35d89bcea2109436cfe2d2d387406a7b32fe
yuya <gurgur.dokan@gmail.com>
2026-07-04 11:45:32 +0900

    setting変更

--- app/settings.py
+++ app/settings.py
@@ -76,20 +76,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'service_db',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'service_db',
-#         'USER': 'django',
-#         'PASSWORD': 'django',
-#         'HOST': 'db',
-#         'PORT': 5432,
-#     }
-# }
 
 
 # Password validation
