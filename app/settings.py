502e0d973401b17704528405a0f1422c5b28939c
yuya <gurgur.dokan@gmail.com>
2026-07-05 17:57:25 +0900

    日記アプリをINSTALLED_APPSに追加

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'dashboard',
+    'diary',
 ]
 
 MIDDLEWARE = [
@@ -84,7 +85,12 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
