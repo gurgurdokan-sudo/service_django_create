b46218580c60cffa369c644ab7ca56f40687e9c2
yuya <gurgur.dokan@gmail.com>
2026-07-05 18:01:55 +0900

    日報一覧へのURLを修正し、正しい名前空間を使用

--- diary/urls.py
+++ diary/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'diary'
 
 urlpatterns = [
-    path('', views.EntryListView.as_view(), name='list'),
+    path('daily_report_list/', views.EntryListView.as_view(), name='list'),
     path('entry/new/', views.EntryCreateView.as_view(), name='create'),
     path('entry/<int:pk>/', views.EntryDetailView.as_view(), name='detail'),
     path('entry/<int:pk>/edit/', views.EntryUpdateView.as_view(), name='update'),
