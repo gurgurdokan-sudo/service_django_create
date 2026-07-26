833f687ecf3249f0ea3d281c78b7ab5ad9c75f67
P2025088 <yuya_itami@tatsuno-gr.com>
2026-07-26 11:13:21 +0900

    日報フォームにユーザー選択フィールドを追加。エントリ詳細と一覧テンプレートにメッセージ表示を追加。

--- diary/forms.py
+++ diary/forms.py
@@ -1,14 +1,22 @@
 from django import forms
 
+from dashboard.models import User
 from .models import Entry
 
 
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
-        fields = ['date', 'title', 'mood', 'image', 'body']
+        # choice = User.objects.filter().values_list('name')
+        fields = ['date', 'title', 'user', 'mood', 'image', 'body']
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
-            'title': forms.TextInput(attrs={'placeholder': '今日の出来事を一言で'}),
-            'body': forms.Textarea(attrs={'rows': 10, 'placeholder': '今日はどんな一日でしたか？'}),
+            'title': forms.TextInput(attrs={'placeholder': '利用者の様子'}),
+            'body': forms.Textarea(attrs={'rows': 10, 'placeholder': '今日の利用者の詳細'}),
+            'user': forms.Select(attrs={'class': 'form-control'}),
         }
+    def __init__(self, *args, **kwargs):
+        super(EntryForm, self).__init__(*args, **kwargs)
+        self.fields['user'].queryset = User.objects.all()
+        self.fields['user'].required = True
+        self.fields['user'].empty_label = "選択してください"
